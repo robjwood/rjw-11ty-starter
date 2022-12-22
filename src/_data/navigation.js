1669dc7db8795706ec2eb29f6fbb5f97f1871322
robjwood <treborwood@gmail.com>
2022-12-22 14:00:31 +0000

    Added netlify.toml

---
+++ src/_data/navigation.js
@@ -0,0 +1,26 @@
+module.exports = {
+  top: [
+    {
+      text: 'Home',
+      url: '/'
+    },
+    {
+      text: 'Page',
+      url: '/page'
+    },
+    {
+      text: 'Section',
+      url: '/section'
+    }
+  ],
+  bottom: [
+    {
+      text: 'Imprint',
+      url: '/imprint/'
+    },
+    {
+      text: 'Privacy',
+      url: '/privacy/'
+    }
+  ]
+};
