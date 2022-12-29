1c257d6c8f51285f01ae6e030d30ad2c6477f163
robjwood <treborwood@gmail.com>
2022-12-29 00:08:32 +0000

    Added 'component' page.

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -9,6 +9,10 @@ module.exports = {
       url: '/elements/'
     },
     {
+      text: 'Components',
+      url: '/components/'
+    },
+    {
       text: 'Section',
       url: '/section/'
     }
